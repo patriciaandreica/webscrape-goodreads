f61211b46dfcc1360b8f97bc83ea5e15496bcc4a
patriciaandreica <68759170+patriciaandreica@users.noreply.github.com>
2022-11-08 17:46:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,109 @@
-import sys
-import json
-
 import requests
-import re
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, redirect, url_for, Response
-from requests.exceptions import HTTPError
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Lists to pass in html
-words = []
+# Dictionary of Headers to send with the Request.
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
+# Lists to pass to html
 error = []
-books = []
+books_give = []
+titles_give = []
+
+# Defining at the module level
+authors = ""
+image = ""
+ratings = ""
+books = ""
+giveaway_titles = ""
+book_input = ""
+
+
+def book_page(searchterms):
+    """
+    Load main page based on book title entered
+    :param searchterms:
+    :return: url of book page
+    """
+    searchterms = "+".join(searchterms.split())
+    url = f'https://www.goodreads.com/search?q={searchterms}'
+    r = requests.get(url, headers={"Content-Type": "text"})
+    soup = BeautifulSoup(r.content, 'html.parser')
+    link = soup.find('a', class_='bookTitle').get('href')
+    url = f'https://www.goodreads.com/{link}'
+    return url
+
+
+# Get author
+def goodreads_author(soup):
+    """
+    Scrape author name of book
+    :param soup:
+    :return: author name as text
+    """
+    author = soup.find('span', class_='ContributorLink__name').get_text()
+    return author
+
+
+# Get book cover
+def goodreads_book_image(soup):
+    """
+    Scrape book image of book
+    :param soup:
+    :return: the image source link
+    """
+    book_image = soup.find('img', class_="ResponsiveImage")
+    return book_image['src']
+
+
+def goodreads_ratings(soup):
+    """
+    Scrape average numeric rating of book
+    :param soup:
+    :return: rating as text
+    """
+    book_rating = soup.find('div', class_="RatingStatistics__rating").get_text()
+    return book_rating
+
+
+def goodreads_giveaway(soup) -> list:
+    """
+    Load page based on book genre of entered book, and scrape
+    book cover
+    :param soup:
+    :return: image source links as a list
+    """
+    url = soup.find('a', class_="Button Button--tag-inline Button--small").get('href')
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    book = soup.find_all("img", class_="bookCover")
+    if books_give:
+        books_give.clear()
+    for element in book:
+        books_give.append(element.attrs['src'])
+    return books_give
+
+
+def goodreads_giveaway_title(soup) -> list:
+    """
+    Load page based on book genre of entered book, and scrape
+    book titles
+    :param soup:
+    :return: book titles text as list
+    """
+    url = soup.find('a', class_="Button Button--tag-inline Button--small").get('href')
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    titles = soup.find_all(class_='bookTitle')
+    if titles_give:
+        titles_give.clear()
+    for i in titles:
+        titles_give.append(i.text)
+    return titles_give
 
 
 @app.route('/')
@@ -21,49 +112,29 @@ def home():
 
 
 @app.route('/index', methods=["POST"])
-def index():
+def index2():
+    global authors
+    global image
+    global ratings
+    global books
+    global giveaway_titles
+    global book_input
     if request.method == 'POST':
         try:
-            genre = request.form["genre"]
-            url = f"https://www.goodreads.com/genres/{genre}"
+            book_input = request.form["book-name"]
+            url = book_page(book_input)
             r = requests.get(url)
             soup = BeautifulSoup(r.content, 'html.parser')
-            # m = soup.find("div", {"class": "giveawayList"})
-            m = soup.find_all(class_='bookTitle')
-
-            # authors = soup.find("div", {"id": "bookAuthors"})
-            # for j in authors:
-            #  author.append(j.text)
-            # book = soup.a.img.find_all(class_='bookCover').src
-
-            book = soup.find_all("img", class_="bookCover")
-            if books:
-                books.clear()
-            for element in book:
-                books.append(element.attrs['src'])
-
-            # final = json.dumps(books)
-            print(books)
-            # print(final)
-            # book = soup.a.img['src']
-            # if dictionary not empty, clear
-            if words or error:
-                words.clear()
-                error.clear()
-            # append text to dictionary
-            for i in m:
-                words.append(i.text)
-            # error with input genre
+            authors = goodreads_author(soup)
+            image = goodreads_book_image(soup)
+            ratings = goodreads_ratings(soup)
+            books = goodreads_giveaway(soup)
+            giveaway_titles = goodreads_giveaway_title(soup)
+            book_input = book_input.capitalize()
         except:
             error.append("Genre not found")
-
-    return render_template("result.html", words=words, error=error, books=books, len=len(books), genre=genre)
-
-
-@app.route('/result', methods=["POST"])
-def result():
-    words = request.form.get("words")
-    return render_template('result.html', words=words)
+    return render_template("result.html", book_input=book_input, authors=authors, image=image, ratings=ratings,
+                           books=books, words=giveaway_titles)
 
 
 if __name__ == '__main__':
